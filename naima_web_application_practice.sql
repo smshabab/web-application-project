3bb3330236710d81519f9842891aecbf302a93e9
S.M.Shabab <42778250+smshabab@users.noreply.github.com>
2021-12-14 21:28:17 +0600

    Update naima_web_application_practice.sql

--- naima_web_application_practice.sql
+++ naima_web_application_practice.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 17, 2021 at 10:23 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.11
+-- Generation Time: Dec 14, 2021 at 04:27 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -41,11 +41,11 @@ CREATE TABLE `dashboard` (
 --
 
 INSERT INTO `dashboard` (`heading_1`, `heading_2`, `heading_3`, `heading_4`, `heading_5`, `id`) VALUES
-(21, 22, 23, 24, 25, 5),
 (26, 27, 28, 29, 30, 6),
 (31, 32, 33, 34, 35, 7),
 (36, 37, 38, 39, 40, 8),
-(41, 42, 43, 44, 45, 9);
+(41, 42, 43, 44, 45, 9),
+(1, 2, 3, 4, 5, 11);
 
 -- --------------------------------------------------------
 
@@ -57,15 +57,17 @@ CREATE TABLE `registration` (
   `serial` int(11) NOT NULL,
   `username` varchar(200) NOT NULL,
   `email` varchar(200) NOT NULL,
-  `password` varchar(200) NOT NULL
+  `password` varchar(200) NOT NULL,
+  `user_type` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `registration`
 --
 
-INSERT INTO `registration` (`serial`, `username`, `email`, `password`) VALUES
-(2, 'naimabintehossain', 'naima@gmail.com', 'pass1234');
+INSERT INTO `registration` (`serial`, `username`, `email`, `password`, `user_type`) VALUES
+(2, 'naimabintehossain', 'naima@gmail.com', 'pass1234', NULL),
+(3, 'smshabab36', 'smshabab36@gmail.com', 'pass1234', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -91,13 +93,13 @@ ALTER TABLE `registration`
 -- AUTO_INCREMENT for table `dashboard`
 --
 ALTER TABLE `dashboard`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `registration`
 --
 ALTER TABLE `registration`
-  MODIFY `serial` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `serial` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
